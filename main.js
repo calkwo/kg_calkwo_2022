251f72d51de17b46d575db8b1bf8e3063c9f44e1
kwokc <calvinusk@gmail.com>
2021-04-28 14:27:55 -0400

    Final Submission

--- main.js
+++ main.js
@@ -2,16 +2,15 @@ var args = process.argv.slice(2);
 var numbers = ["Zero", "One", "Two", "Three", "Four",
                "Five", "Six", "Seven", "Eight", "Nine"];
 var output = "";
-
 var first = true;
-for (let arg of args){
-    if (first){
+for (let arg of args) {
+    if (first) {
         first = 0;
     }
-    else{
+    else {
         output+=',';
     }
-    for (let num of arg){
+    for (let num of arg) {
         output+=numbers[parseInt(num)];
     }
 }
