3ab08a82c4b1b8c5c4475c3b739b76a7f3df0cc8
kwokc <calvinusk@gmail.com>
2021-04-28 13:53:12 -0400

    compacted code into one print statement

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 var args = process.argv.slice(2);
 var numbers = ["Zero", "One", "Two", "Three", "Four",
                "Five", "Six", "Seven", "Eight", "Nine"];
+var output = "";
 
 var first = true;
 for (let arg of args){
-    var output = ",";
     if (first){
-        output = "";
         first = 0;
+    }
+    else{
+        output+=',';
     }
     for (let num of arg){
         output+=numbers[parseInt(num)];
     }
-    process.stdout.write(output);
 }
-process.stdout.write("\n");
+process.stdout.write(output + '\n');
