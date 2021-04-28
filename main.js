3dcc52dc0e9fc102722f862a2807481b92d46d00
kwokc <calvinusk@gmail.com>
2021-04-28 13:13:09 -0400

    added commas and var first

--- main.js
+++ main.js
@@ -2,8 +2,13 @@ var args = process.argv.slice(2);
 var numbers = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"];
 //console.log(args);
 
+var first = true;
 for (let arg of args){
-    var output = "";
+    var output = ",";
+    if (first){
+        output = "";
+        first = 0;
+    }
     for (let num of arg){
         output+=numbers[parseInt(num)];
     }
