d697da635347cdb9b9ba256ae03bec841dec122b
kwokc <calvinusk@gmail.com>
2021-04-28 13:11:41 -0400

    added correct print for each number

---
+++ main.js
@@ -0,0 +1,11 @@
+var args = process.argv.slice(2);
+var numbers = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"];
+//console.log(args);
+
+for (let arg of args){
+    var output = "";
+    for (let num of arg){
+        output+=numbers[parseInt(num)];
+    }
+    process.stdout.write(output);
+}
