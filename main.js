a07aea9a78c393caa358165c53bafed1fc085551
kwokc <calvinusk@gmail.com>
2021-04-28 14:29:09 -0400

    Final Submission

---
+++ main.js
@@ -0,0 +1,17 @@
+var args = process.argv.slice(2);
+var numbers = ["Zero", "One", "Two", "Three", "Four",
+               "Five", "Six", "Seven", "Eight", "Nine"];
+var output = "";
+var first = true;
+for (let arg of args) {
+    if (first) {
+        first = 0;
+    }
+    else {
+        output+=',';
+    }
+    for (let num of arg) {
+        output+=numbers[parseInt(num)];
+    }
+}
+process.stdout.write(output + '\n');
